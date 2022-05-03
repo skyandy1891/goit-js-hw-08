c69228457ce6b0aa4c6b461f000db3f51e67b7d9
skyandy1891 <skyandy1891@gmail.com>
2022-05-04 00:27:00 +0300

    add galery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
+const img = creategalleryItems(galleryItems);
+gallery.addEventListener("click", container);
+gallery.insertAdjacentHTML("beforeend", img);
+
+
+function creategalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+    })
+    .join("");
+}
+
+function container(e) {
+  e.preventDefault();
+
+  if (!e.target.dataset.source) {
+    return;
+  }
+  const card = e.target.dataset.source;
+  console.log("cardUrl", card);
+  const span = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="100" height="50">
+`);
+  span.show();
+}
 console.log(galleryItems);
+// Change code above this line
